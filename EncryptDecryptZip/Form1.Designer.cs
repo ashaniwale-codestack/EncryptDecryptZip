b321e5a92a23ff7e496a752d629ef6c6b3dadbd9
Asha Niwale <ashniwale248@gmail.com>
2024-06-16 23:36:03 +0530

    initial commit

---
+++ EncryptDecryptZip/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace EncryptDecryptZip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fdlg = new System.Windows.Forms.OpenFileDialog();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOutputPath = new System.Windows.Forms.Button();
+            this.fdlgOutputPath = new System.Windows.Forms.OpenFileDialog();
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(500, 172);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(415, 34);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter file path *";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilePath.Location = new System.Drawing.Point(507, 109);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(306, 24);
+            this.lblFilePath.TabIndex = 2;
+            this.lblFilePath.Text = "Your file path will be displayed here";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(126, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter file password *";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(500, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Click to select a file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fdlg
+            // 
+            this.fdlg.FileName = "openFileDialog1";
+            this.fdlg.FileOk += new System.ComponentModel.CancelEventHandler(this.fdlg_FileOk);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(289, 364);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(162, 48);
+            this.btnEncrypt.TabIndex = 5;
+            this.btnEncrypt.Text = "Encrypt File";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(552, 364);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(153, 48);
+            this.btnDecrypt.TabIndex = 6;
+            this.btnDecrypt.Text = "Decrypt FIle";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 264);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(269, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Path to save output file *";
+            // 
+            // btnOutputPath
+            // 
+            this.btnOutputPath.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnOutputPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOutputPath.Location = new System.Drawing.Point(500, 265);
+            this.btnOutputPath.Name = "btnOutputPath";
+            this.btnOutputPath.Size = new System.Drawing.Size(205, 33);
+            this.btnOutputPath.TabIndex = 8;
+            this.btnOutputPath.Text = "Click here to select path";
+            this.btnOutputPath.UseVisualStyleBackColor = false;
+            this.btnOutputPath.Click += new System.EventHandler(this.btnOutputPath_Click);
+            // 
+            // fdlgOutputPath
+            // 
+            this.fdlgOutputPath.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 563);
+            this.Controls.Add(this.btnOutputPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog fdlg;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOutputPath;
+        private System.Windows.Forms.OpenFileDialog fdlgOutputPath;
+        private System.Windows.Forms.FolderBrowserDialog fbd1;
+    }
+}
+
---
+++ EncryptDecryptZip/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptDecryptZip
+{
+    public partial class Form1 : Form
+    {
+        public string outputFilePath = string.Empty;
+        public string fileName = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                lblFilePath.Text = fdlg.FileName;
+                fileName = fdlg.SafeFileName.Split('.')[0];
+            }
+        }
+
+        private void fdlg_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtPassword.Text == string.Empty || fileName == string.Empty || fdlgOutputPath == null)
+                {
+                    MessageBox.Show("Please enter all the required details.");
+                }
+                else
+                {
+                    string password = txtPassword.Text; // Your Key Here
+                    UnicodeEncoding UE = new UnicodeEncoding();
+                    byte[] key = UE.GetBytes(password);
+
+                    string cryptFile = outputFilePath + "\\" + fileName + "_Enc.zip";
+                    FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                    RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                    CryptoStream cs = new CryptoStream(fsCrypt,
+                        RMCrypto.CreateEncryptor(key, key),
+                        CryptoStreamMode.Write);
+
+                    FileStream fsIn = new FileStream(lblFilePath.Text, FileMode.Open);
+
+                    int data;
+                    while ((data = fsIn.ReadByte()) != -1)
+                        cs.WriteByte((byte)data);
+
+                    fsIn.Close();
+                    cs.Close();
+                    fsCrypt.Close();
+                    MessageBox.Show("File Encrypted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Some error occurred, please try again later.");
+            }
+            finally
+            {
+                resetForm();
+            }
+        }
+
+        private void btnOutputPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd1 = new FolderBrowserDialog();
+            if (fbd1.ShowDialog() == DialogResult.OK)
+            {
+                outputFilePath = fbd1.SelectedPath;
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtPassword.Text == string.Empty || fileName == string.Empty || fdlgOutputPath == null)
+                {
+                    MessageBox.Show("Please enter all the required details.");
+                }
+                else
+                {
+                    string password = txtPassword.Text; // Your Key Here
+
+                    UnicodeEncoding UE = new UnicodeEncoding();
+                    byte[] key = UE.GetBytes(password);
+
+                    FileStream fsCrypt = new FileStream(lblFilePath.Text, FileMode.Open);
+
+                    RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                    CryptoStream cs = new CryptoStream(fsCrypt,
+                        RMCrypto.CreateDecryptor(key, key),
+                        CryptoStreamMode.Read);
+
+                    FileStream fsOut = new FileStream(outputFilePath + "\\" + fileName +  "_Dec.zip", FileMode.Create);
+
+                    int data;
+                    while ((data = cs.ReadByte()) != -1)
+                        fsOut.WriteByte((byte)data);
+
+                    fsOut.Close();
+                    cs.Close();
+                    fsCrypt.Close();
+                    MessageBox.Show("File Decrypted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Some error occurred, please try again later.");
+            }
+            finally
+            {
+                resetForm();
+            }
+        }
+        private void resetForm()
+        {
+            lblFilePath.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            outputFilePath = string.Empty;
+            fbd1.SelectedPath = string.Empty;
+            fdlg.FileName = string.Empty;
+            fileName = string.Empty;
+        }
+    }
+}
